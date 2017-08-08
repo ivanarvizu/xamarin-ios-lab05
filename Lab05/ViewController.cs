f5b935a76c4ed5860b19dceac84e91d64534f7ca
Iván Arvizu <ivanarvizu@outlook.com>
2017-08-08 14:14:31 -0700

    Agregado iconos

--- Lab05/ViewController.cs
+++ Lab05/ViewController.cs
@@ -13,6 +13,36 @@ namespace Lab05
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+            var TranslatedNumber = string.Empty;
+
+            TranslateButton.TouchUpInside += (object sender, EventArgs e) => {
+                var Translator = new PhoneTranslator();
+                TranslatedNumber = Translator.ToNumber(PhoneNumberText.Text);
+                if(string.IsNullOrWhiteSpace(TranslatedNumber))
+                {
+                    CallButton.SetTitle("Llamar", UIControlState.Normal);
+                    CallButton.Enabled = false;
+                }
+                else
+                {
+                    CallButton.SetTitle($"Llamar al {TranslatedNumber}", UIControlState.Normal);
+                    CallButton.Enabled = true;
+                }
+            };
+
+            CallButton.TouchUpInside += (object sender, EventArgs e) => {
+                var URL = new Foundation.NSUrl($"tel:{TranslatedNumber}");
+
+                if(!UIApplication.SharedApplication.OpenUrl(URL))
+                {
+                    var Alert = UIAlertController.Create("No soportado"
+                        , "El esquema 'tel:' no es soportado en este dispositivo"
+                        , UIAlertControllerStyle.Alert
+                        );
+                    Alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+                    PresentViewController(Alert, true, null);
+                }
+            };
             // Perform any additional setup after loading the view, typically from a nib.
         }
 
--- Lab05/ViewController.designer.cs
+++ Lab05/ViewController.designer.cs
@@ -1,17 +1,50 @@
-﻿//
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace Lab05
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton CallButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PhoneNumberText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton TranslateButton { get; set; }
+
+        [Action ("UIButton7_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton7_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
         {
+            if (CallButton != null) {
+                CallButton.Dispose ();
+                CallButton = null;
+            }
+
+            if (PhoneNumberText != null) {
+                PhoneNumberText.Dispose ();
+                PhoneNumberText = null;
+            }
+
+            if (TranslateButton != null) {
+                TranslateButton.Dispose ();
+                TranslateButton = null;
+            }
         }
     }
 }
